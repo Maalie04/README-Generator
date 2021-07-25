1111e9db7dd65b0fa0bcdc260af188c2bdb5f077
Jamaal Lawson <maaliemaal@gmail.com>
2021-07-25 18:06:57 -0400

    added another question in my prompt that ask for the title of the app, then writing code to create and write onto a file

--- index.js
+++ index.js
@@ -1,75 +1,90 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateMarkDown = require('./generateMarkDown');
+
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => { 
-return inquirer.prompt([
-    {
-    type: "input",
-    name: "Description",
-    message: "Tell us about the application"
-},
-{
-    type: "list",
-    name: "table of contents",
-    message: ""
-},
-{
-    type: "input",
-    name: "Installation",
-    message: ""
-},
-{
-    type: "input",
-    name: "Usage",
-    message: ""
-},
-{
-    type: "input",
-    name: "Contributions",
-    message: "Who are the contribitors?"
-},
-{
-    type: "input",
-    name: "Test",
-    message: ""
-},
-{
-    type: "list",
-    name: "Licence",
-    message: "What license did you choose?",
-    choices: ['Apache','MIT License', 'Mozilla Public Liscense' ],
-    validate: (value) => {if(value){return true}else{return 'Please choose a license!!'}}
-},
-{
-    type: "input",
-    name: "Github",
-    message: "Enter Github..."
-},
-{
-    type: "input",
-    name: "Linkedin",
-    message: "Enter Linkedin..."
-},
-{
-    type: "input",
-    name: "Email",
-    message: "What is your Email?"
-},
-]) .then(function(answers){
-    
-})
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "What is the Title of the application"
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "Tell us about the application"
+        },
+        {
+            type: "list",
+            name: "table of contents",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "Contributions",
+            message: "Who are the contribitors?"
+        },
+        {
+            type: "input",
+            name: "Test",
+            message: ""
+        },
+        {
+            type: "list",
+            name: "Licence",
+            message: "What license did you choose?",
+            choices: ['Apache', 'MIT License', 'Mozilla Public Liscense'],
+            validate: (value) => { if (value) { return true } else { return 'Please choose a license!!' } }
+        },
+        {
+            type: "input",
+            name: "Github",
+            message: "Enter Github..."
+        },
+        {
+            type: "input",
+            name: "Linkedin",
+            message: "Enter Linkedin..."
+        },
+        {
+            type: "input",
+            name: "Email",
+            message: "What is your Email?"
+        },
+    ]).then(function (answers) {
+
+    })
 };
 
 const writeToFile = (filename, data) => {
-    
+
+    const filename = `${data.Title.toLowerCase().split(' ').join('')}.json`;
+    fs.writeFile("./assets" + filename, data, function (err) {
+        if (err) {
+            console.log(err)
+        }
+    })
 };
 
 const init = () => {
     promptUser()
-    .then(())
+        .then(function (data) {
+            writeToFile('readMe.md', generateMarkDown(data));
+        })
 };
 
 
